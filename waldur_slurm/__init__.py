f259f586528805ecdecdfbeaba159c7541c03f4d
Sergei Zaiaev <sergei.zaiaev@ut.ee>
2023-01-25 14:42:50 +0000

    Add sentry support

--- waldur_slurm/__init__.py
+++ waldur_slurm/__init__.py
@@ -59,3 +59,12 @@ if WALDUR_SLURM_USERNAME_SOURCE not in [
 waldur_rest_client = WaldurClient(WALDUR_API_URL, WALDUR_API_TOKEN)
 
 slurm_backend = SlurmBackend()
+
+sentry_dsn = os.environ.get("SENTRY_DSN")
+
+if sentry_dsn:
+    import sentry_sdk
+
+    sentry_sdk.init(
+        dsn=sentry_dsn,
+    )
